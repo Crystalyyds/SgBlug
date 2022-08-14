8cc12543e6c11953d939e4f952ae7a6eec3f79b5
Lei.Yang <2031291160@qq.com>
2022-08-14 16:06:55 +0800

    test1

---
+++ src/main/java/com/github/sgblug/app/controller/Test.java
@@ -0,0 +1,11 @@
+package com.github.sgblug.app.controller;
+
+/**
+ * @author 20312
+ * @author 2022/8/14 16:05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test....");
+    }
+}
